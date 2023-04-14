5fb8b5cd0cc2816fc5c18a49bb159683c89e8687
Olayinka Oniyide <oniyideolayinka97@gmail.com>
2023-04-14 14:36:07 +0100

    initial commit

---
+++ ProblemSolving/PrintSemicolon.py
@@ -0,0 +1,8 @@
+word = "SEMICOLON"
+for letter in word:
+    print(letter)
+
+
+words = "Ventures"
+for letter in words:
+    print(letter)
---
+++ ProblemSolving/printVentures.py
@@ -0,0 +1,4 @@
+word = "VENTURES"
+
+for i in range(0, len(word), 2):
+    print(word[i:i+2])
